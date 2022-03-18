3bead5a5150a85a48c17a38d81114689f7d8f25b
Itgotworse26 <alvin.tran1997@gmail.com>
2022-03-17 19:35:21 -0700

    Add updated schema with join queries.

--- schema.sql
+++ schema.sql
@@ -24,3 +24,21 @@ CREATE TABLE other_cars_details (
     FOREIGN KEY (car_id) REFERENCES cars_details (car_id),
     PRIMARY KEY (car_id)
 );
+
+SELECT * from other_cars_details;
+
+SELECT c.car_id,
+	c.name,
+	c.year,
+	c.selling_price,
+	c.fuel,
+	c.seller_type,
+	o.km_driven,
+	o.owner,
+	o.mileage
+INTO cars_summary
+FROM cars_details as c
+INNER JOIN other_cars_details as o
+ON c.car_id = o.car_id
+
+SELECT * from cars_summary;
