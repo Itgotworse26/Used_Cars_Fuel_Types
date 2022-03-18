e35017a5be1fa7fb143645d301a12580314e32e9
afreen amina <afreenshabeer@gmail.com>
2022-03-17 19:17:57 -0700

    Adding Schema sql query

---
+++ Database/schema.sql
@@ -0,0 +1,26 @@
+CREATE TABLE cars_details (
+    car_id VARCHAR NOT NULL,
+    name VARCHAR NOT NULL,
+    year INT NOT NULL,
+    selling_price INT NOT NULL,
+    fuel VARCHAR NOT NULL,
+    seller_type VARCHAR NOT NULL,
+    seats INT,
+    PRIMARY KEY (car_id)
+	);
+	
+SELECT * from cars_details;
+
+CREATE TABLE other_cars_details (
+    car_id VARCHAR NOT NULL,
+    name VARCHAR NOT NULL,
+    km_driven INT NOT NULL,
+    transmission VARCHAR NOT NULL,
+    owner VARCHAR NOT NULL,	
+    mileage VARCHAR,
+    engine VARCHAR,
+    max_power VARCHAR,
+    torque VARCHAR,
+    FOREIGN KEY (car_id) REFERENCES cars_details (car_id),
+    PRIMARY KEY (car_id)
+);
